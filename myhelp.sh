7b51f69a8961d4e943d3c1a91cd33776fa012217
rbriggs93 <romankaftekt@gmail.com>
2026-04-22 18:55:54 -0700

    Add files via upload

--- myhelp.sh
+++ myhelp.sh
@@ -4,6 +4,7 @@
 
 #Config
 helpdir="${HOME}/.help"
+edit='/usr/bin/nano'
 
 # Init
 if [[ ! -d "$helpdir" ]]; then
@@ -11,41 +12,41 @@ if [[ ! -d "$helpdir" ]]; then
 fi
 
 get_helpfiles () {
-    find "$helpdir" -maxdepth 1 -mindepth 1 ! -name ".*" -printf "  %f\n"
+  find "$helpdir" -maxdepth 1 -mindepth 1 ! -name ".*" -printf "  %f\n"
 exit 0; }
 
 do_usage () {
-	echo "Usage:"
-	echo "  myhelp    <helpfile>"
-	echo "  myhelp -n <helpfile>"
-	echo "  myhelp -e <helpfile>"
-	echo ""
-	echo "Options:"
-	echo "  -n|--new  : create new helpfile"
-	echo "  -e|--edit : edit existing helpfile"
-	echo "  -l|--list : list existing helpfiles"
-	echo "  -h|--help : this help message"
-	echo ""
-    echo "Available helpfile entries:"
-    get_helpfiles
+  echo "Usage:"
+  echo "  myhelp    <helpfile>"
+  echo "  myhelp -n <helpfile>"
+  echo "  myhelp -e <helpfile>"
+  echo ""
+  echo "Options:"
+  echo "  -n|--new  : create new helpfile"
+  echo "  -e|--edit : edit existing helpfile"
+  echo "  -l|--list : list existing helpfiles"
+  echo "  -h|--help : this help message"
+  echo ""
+  echo "Available helpfile entries:"
+  get_helpfiles
 }
 
 new_helpfile () {
-    local confirm="${confirm:-false}"
-    if [ "$confirm" = true ]; then
-      r=''
-      read -r -p "$1 has no help file. Create one? [Y/n] > " r
-      case "$r" in
-        y|Y|"${NULL}")
-        ;;
-        *)
-          echo "kthxbai" && exit 1
-        ;;
-      esac
-    fi
-    touch "$helpdir/$1" || ( echo "Cannot write $helpdir/$1!" && exit 255 )
-    sleep 0.1
-    nano "$helpdir/$1"
+  local confirm="${confirm:-false}"
+  if [ "$confirm" = true ]; then
+    r=''
+    read -r -p "$helpdir/$1 has no help file. Create one? [Y/n] > " r
+    case "$r" in
+      y|Y)
+      ;;
+      *)
+        echo "User declined to create helpfile." && exit 1
+      ;;
+    esac
+  fi
+  touch "$helpdir/$1" || ( echo "Cannot write $helpdir/$1!" && exit 255 )
+  sleep 0.1
+  $edit "$helpdir/$1"
 exit 0; }
 
 # Check if we were called without an option nor a file
@@ -53,35 +54,34 @@ exit 0; }
 
 # Check if we have a valid option
 case "$1" in
-    -h|--help)
-      do_usage
-    ;;
-    -l|--list)
-      get_helpfiles
-    ;;
-    -n|--new)
-      if   [[ -f "$helpdir/$2" ]]
-      then echo  "Help file $helpdir/$2 exists!" && exit 1
-      else new_helpfile "$2"
-      fi
-    ;;
-    -e|--edit)
-      if   [[ -f "$helpdir/$2" ]]
-      then nano  "$helpdir/$2" || exit 0
-      else confirm=true new_helpfile
-      fi
-    ;;
-    # Option, but not valid: error
-    -*)
-      echo "Unrecognized option!" && exit 1
-    ;;
-    *)
-    # No option: fall through
-    ;;
+  -h|--help)
+    do_usage
+  ;;
+  -l|--list)
+    get_helpfiles
+  ;;
+  -n|--new)
+    if   [[ -f "$helpdir/$2" ]]
+    then echo  "Help file $helpdir/$2 exists!" && exit 1
+    else new_helpfile "$2"
+    fi
+  ;;
+  -e|--edit)
+    if   [[ -f "$helpdir/$2" ]]
+    then $edit  "$helpdir/$2" || exit 0
+    else confirm=true new_helpfile
+    fi
+  ;;
+  # Option, but not valid: error
+  -*)
+    echo "Unrecognized option!" && exit 1
+  ;;
+  *) # No option: fall through
+  ;;
 esac
 
 # Check if we are referencing an existing file, or offer to create one
 [[ -f "$helpdir/$1" ]] || confirm=true new_helpfile "$1"
-nano "$helpdir/$1"
+$edit "$helpdir/$1"
 
 exit 0
